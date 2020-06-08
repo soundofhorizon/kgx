ad106a2dd67ebfaa048380235dfdff1e0303ce7e
soundofhorizon <ooshiba0206@gmail.com>
2020-06-09 02:57:44 +0900

    [FIX]content抜けてるやつ～ｗｗｗｗ

--- cogs/message.py
+++ cogs/message.py
@@ -277,7 +277,7 @@ class Message(commands.Cog):
                         # フォーマットされたdatetimeとの変換を試みTrueかどうかを調べる
                         return fourth_user_input.channel == ctx.channel and re.match(
                             r"[0-9]{4}/[0-9]{2}/[0-9]{2}-[0-9]{2}:[0-9]{2}",
-                            fourth_user_input.content) and datetime.strftime(fourth_user_input, "%Y/%m/%d-%H:%M")
+                            fourth_user_input.content) and datetime.strftime(fourth_user_input.content, "%Y/%m/%d-%H:%M")
 
                 # 価格フォーマットチェック
                 def check3(m):
@@ -406,7 +406,7 @@ class Message(commands.Cog):
                     # フォーマットされたdatetimeとの変換を試みTrueかどうかを調べる
                     return fourth_user_input.channel == ctx.channel and re.match(
                         r'[0-9]{4}/[0-9]{2}/[0-9]{2}-[0-9]{2}:[0-9]{2}',
-                        fourth_user_input.content) and datetime.strftime(fourth_user_input, "%Y/%m/%d-%H:%M")
+                        fourth_user_input.content) and datetime.strftime(fourth_user_input.content, "%Y/%m/%d-%H:%M")
 
                 # 価格フォーマットチェック
                 def check3(m):
