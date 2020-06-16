fd9d899e402498d9d2d879e34b3a27bd52404fa5
soundofhorizon <ooshiba0206@gmail.com>
2020-06-16 11:49:01 +0900

    [FIX]self.bot

--- cogs/admin_only.py
+++ cogs/admin_only.py
@@ -143,7 +143,10 @@ class AdminOnly(commands.Cog):
                     str = ""
                 else:
                     data = r.get(i).decode().split(",")
-                    str += f"INSERT INTO bid_ranking VALUES ('{data[0]}', '{data[1]}', {data[2]}, {self.bot.get_user(data[3]).display_name} );\n"
+                    if self.bot.get_user(int(data[3])):
+                        str += f"INSERT INTO bid_ranking VALUES ('{data[0]}', '{data[1]}', {data[2]}, {self.bot.get_user(int(data[3])).display_name} );\n"
+                    else:
+                        str += f"INSERT INTO bid_ranking VALUES ('{data[0]}', '{data[1]}', {data[2]}, {data[3]} );\n"
                     i += 1
             else:
                 break
