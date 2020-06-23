8021775c1247ff6ff9fe7b9497bbb1fe42b612fd
soundofhorizon <ooshiba0206@gmail.com>
2020-06-23 17:55:21 +0900

    [ADD]bid処理を流す前段階

--- cogs/loops.py
+++ cogs/loops.py
@@ -1,5 +1,13 @@
+import datetime
+import os
+
+import psycopg2
 from discord.ext import commands, tasks
 import discord
+
+SQLpath = os.environ["DATABASE_URL"]
+db = psycopg2.connect(SQLpath)  # sqlに接続
+cur = db.cursor()  # なんか操作する時に使うやつ
 
 
 class Loops(commands.Cog):
@@ -12,6 +20,14 @@ class Loops(commands.Cog):
         await self.bot.wait_until_ready()
         game = discord.Game(f"{self.bot.get_guild(558125111081697300).member_count}人を監視中")
         await self.bot.change_presence(status=discord.Status.online, activity=game)
+
+    @tasks.loop(seconds=60)
+    async def bid_task(self):
+        # 終わるの早いやつ1つ取ってくる
+        cur.execute("SELECT * from auction ORDER BY auction_end_time ASC")
+        auction_data = cur.fetchone()
+        if auction_data[6] >= datetime.datetime.now():
+            await self.bot.get_channel(id=int(auction_data[0])).send("終わりです。")
 
 
 def setup(bot):
