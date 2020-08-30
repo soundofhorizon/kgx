ad36d7c3fbe8b430041b75a5b13fb26855a5bb3c
soundofhorizon <ooshiba0206@gmail.com>
2020-08-30 15:45:15 +0900

    [DEBUG]

--- cogs/auction_deal.py
+++ cogs/auction_deal.py
@@ -705,7 +705,8 @@ class AuctionDael(commands.Cog):
                 if i == 0:
                     continue
                 else:
-                    discription += f"{i}: {self.bot.get_user(id=tendrs_data[i]).display_name}, {self.bot.stack_check_reverse(tend_prices[i])}"
+                    await ctx.channel.send(f"{i}: {self.bot.get_user(id=tendrs_data[i]).display_name}, {self.bot.stack_check_reverse(tend_prices[i])}\n\n")
+                    discription += f"{i}: {self.bot.get_user(id=tendrs_data[i]).display_name}, {self.bot.stack_check_reverse(tend_prices[i])}\n\n"
 
                 if len(discription) >= 1800:
                     await ctx.channel.send(embed=discord.Embed(discription=discription, color=0xffaf60))
