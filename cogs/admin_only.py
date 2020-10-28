38732509ada4e11126ab9c2c1a940fae11b459b4
soundofhorizon <ooshiba0206@gmail.com>
2020-10-29 02:46:13 +0900

    [FIX]design

--- cogs/admin_only.py
+++ cogs/admin_only.py
@@ -319,30 +319,66 @@ class AdminOnly(commands.Cog):
         else:
             await ctx.send(f"{ctx.prefix}dbsetup [a, d]")
 
+    # auctionのやつ
     @commands.command()
     async def test(self, ctx):
         cur.execute("SELECT DISTINCT auction.ch_id, auction.auction_owner_id, auction.auction_item, tend.tender_id, "
-                    "auction.unit, tend.tend_price FROM (auction JOIN tend ON auction.ch_id = tend.ch_id)")
+                    "auction.unit, tend.tend_price, auction.auction_end_time FROM "
+                    "(auction JOIN tend ON auction.ch_id = tend.ch_id)")
         data = cur.fetchall()
         description = ""
         for i in range(len(data)):
-            description += f"{self.bot.get_channel(id=data[i][0]).name}\n"
-        await ctx.channel.send(description)
-        description = ""
-        for i in range(len(data)):
-            if data[i][1] == 0:
+            # debug出てもらっても困るので消滅させる。
+            if data[i][0] == 747728655735586876:
+                continue
+            # オークションが開催されてないときdisplay_nameが取れない。(人いないし)よって分岐
+            elif data[i][1] == 0:
                 description += f"{self.bot.get_channel(id=data[i][0]).name}:\n"
                 description += f"   現在このチャンネルでオークションは開催していません！\n"
+            # 他記述。
             else:
                 description += f"{self.bot.get_channel(id=data[i][0]).name}:\n"
                 description += f"   出品者 → {self.bot.get_user(id=data[i][1]).display_name}\n"
                 description += f"   商品名 → {data[i][2]}\n"
+                # 多分no bidで更新すると死ぬ気がするので分岐
                 if data[i][3][-1] == 0:
                     description += "    入札者はまだいません！\n"
                 else:
                     description += f"   最高額入札者 → {self.bot.get_user(id=data[i][3][-1]).display_name}\n"
                     description += f"   入札額 → {data[i][4]}{self.bot.stack_check_reverse(data[i][5][-1])}\n"
+                description += f"   終了時刻 → {data[i][6]}\n"
             description += "\n--------\n\n"
+
+            # 文字数制限回避。多分足りない
+            if len(description) >= 1800:
+                embed = discord.Embed(description=description, color=0x59a5e3)
+                await ctx.channel.send(embed=embed)
+                description = ""
+
+        embed = discord.Embed(description=description, color=0x59a5e3)
+        await ctx.channel.send(embed=embed)
+
+    # dealのやつ
+    @commands.command()
+    async def test2(self, ctx):
+        cur.execute("SELECT * from deal")
+        data = cur.fetchall()
+        description = ""
+        for i in range(len(data)):
+            # 取引が開催されてないときdisplay_nameが取れない。(人いないし)よって分岐
+            if data[i][1] == 0:
+                description += f"{self.bot.get_channel(id=data[i][0]).name}:\n"
+                description += f"   現在このチャンネルで取引は開催していません！\n"
+            # 他記述。
+            else:
+                description += f"{self.bot.get_channel(id=data[i][0]).name}:\n"
+                description += f"   出品者 → {self.bot.get_user(id=data[i][1]).display_name}\n"
+                description += f"   商品名 → {data[i][3]}\n"
+                description += f"   出品価格 → {data[i][6]}{self.bot.stack_check_reverse(data[i][4])}\n"
+                description += f"   出品価格 → {data[i][5]}\n"
+            description += "\n--------\n\n"
+
+            # 文字数制限回避。多分足りない
             if len(description) >= 1800:
                 embed = discord.Embed(description=description, color=0x59a5e3)
                 await ctx.channel.send(embed=embed)
