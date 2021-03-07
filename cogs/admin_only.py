a48e55b6a40c7cc101316ad7716aaf51184ba70d
soundofhorizon <ooshiba0206@gmail.com>
2021-03-08 03:57:21 +0900

    [ADD]取引状況をトラッキング

--- cogs/admin_only.py
+++ cogs/admin_only.py
@@ -257,6 +257,95 @@ class AdminOnly(commands.Cog):
 
         await ctx.channel.send(f"{role.mention}持ちの{n}人を吹き飛ばしました")
 
+    @commands.command()
+    async def test(self):
+        deal_data_channel = self.bot.get_channel(id=771068489627861002)
+        await deal_data_channel.purge(limit=100)
+        cur.execute("SELECT * from deal")
+        sql_data = cur.fetchall()
+
+        description = ""
+        before_sort_data = []
+        # [ch_id, ch_name, data]の2重リストを作成し、ch_nameを基準に変更を加える。いい方法があったら変更してほしい><
+        for i in range(len(sql_data)):
+            # 椎名debug
+            if sql_data[i][0] == 747728655735586876:
+                continue
+            else:
+                before_sort_data.append([sql_data[i][0], self.bot.get_channel(id=sql_data[i][0]).name, sql_data[i]])
+
+        # マジでここのアルゴリズムを変えたい
+        # 椎名かガチャ券かなどを分類(ついでに開催されてるオークション数を調べる)
+        siina_ch = []
+        gatya_ch = []
+        all_ch = []
+        yami_ch = []
+        active_auction = 0
+        for i in range(len(before_sort_data)):
+            if "椎名" in before_sort_data[i][1]:
+                siina_ch.append(before_sort_data[i])
+            elif "ガチャ券" in before_sort_data[i][1]:
+                gatya_ch.append(before_sort_data[i])
+            elif "all" in before_sort_data[i][1]:
+                all_ch.append(before_sort_data[i])
+            elif "闇取引" in before_sort_data[i][1]:
+                yami_ch.append(before_sort_data[i])
+            if not before_sort_data[i][2][1] == 0:
+                active_auction += 1
+        # 椎名 - ガチャ券 - all - 闇取引の順で正規表現で殴りラムダ式で並び替え
+        siina_ch_sorted = sorted(siina_ch, reverse=False, key=lambda x: int(re.search(r'\d+', x[1]).group()))
+        gatya_ch_sorted = sorted(gatya_ch, reverse=False, key=lambda x: int(re.search(r'\d+', x[1]).group()))
+        all_ch_sorted = sorted(all_ch, reverse=False, key=lambda x: int(re.search(r'\d+', x[1]).group()))
+        yami_ch_sorted = sorted(yami_ch, reverse=False, key=lambda x: int(re.search(r'\d+', x[1]).group()))
+        data = []
+        for i in siina_ch_sorted:
+            data.append(i)
+        for i in gatya_ch_sorted:
+            data.append(i)
+        for i in all_ch_sorted:
+            data.append(i)
+        for i in yami_ch_sorted:
+            data.append(i)
+
+        showed_active_auction = 0
+        for i in range(len(data)):
+            # owner_id = 0 -> 開催してない
+            if data[i][2][1] == 0:
+                continue
+            # 他記述。
+            else:
+                showed_active_auction += 1
+                # 終了時刻までの残り時間を計算
+                now = datetime.datetime.now()
+                check = datetime.datetime.strptime(data[i][2][5], "%Y/%m/%d-%H:%M")
+                diff = check - now
+                diff_hours = int(diff.seconds/3600)
+                diff_minites = int((diff.seconds - diff_hours*3600)/60)
+                diff_seconds = diff.seconds - diff_hours*3600 - diff_minites*60
+
+                description += f"{self.bot.get_channel(id=data[i][2][0]).mention}:\n"
+                description += f"   出品者 → {self.bot.get_user(id=data[i][2][1]).display_name}\n"
+                description += f"   商品名 → {data[i][2][2]}\n"
+                description += f"   希望価格 → {data[i][2][6]}{self.bot.stack_check_reverse(data[i][2][4])}\n"
+                if diff_hours == 0:
+                    description += f"   終了まで残り → **{diff.days}日{diff_hours}時間{diff_minites}分{diff_seconds}秒**\n"
+                else:
+                    description += f"   終了まで残り → {diff.days}日{diff_hours}時間{diff_minites}分{diff_seconds}秒\n"
+            if active_auction >= 2 and showed_active_auction < active_auction:
+                description += "\n--------\n\n"
+
+            # 文字数制限回避。多分足りない
+            if len(description) >= 1800:
+                embed = discord.Embed(description=description, color=0x59a5e3)
+                await deal_data_channel.send(embed=embed)
+                description = ""
+
+        embed = discord.Embed(description="取引はまだ一つも行われていません！", color=0x59a5e3)
+        if not len(description) == 0:
+            embed = discord.Embed(description=description, color=0x59a5e3)
+        await deal_data_channel.send(embed=embed)
+
+
 
 def setup(bot):
     bot.add_cog(AdminOnly(bot))
