ffcb64ed8967a4b41b14ea8a1aa173e4842f2de2
conarin <53005059+conarin@users.noreply.github.com>
2021-05-08 00:22:42 +0900

    [FIX]old_scoreをint型に変更

--- cogs/auction_deal.py
+++ cogs/auction_deal.py
@@ -33,8 +33,8 @@ class AuctionDael(commands.Cog):
         p = re.compile(r'^[0-9]+$')
         if p.fullmatch(str(pt)):
             cur.execute("SELECT bid_score FROM user_data where user_id = %s", (ctx.author.id,))
-            old_score = list(cur.fetchone())
-            new_score = old_score[0] + pt
+            old_score = list(cur.fetchone())[0]
+            new_score = old_score + pt
             cur.execute("UPDATE user_data SET bid_score = %s WHERE user_id = %s", (new_score, ctx.author.id))
             db.commit()
 
