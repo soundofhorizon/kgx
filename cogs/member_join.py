6360ad0bf75f5e62cedbde1df14a2b6a7afdbe75
kaeru82433413 <56305929+kaeru82433413@users.noreply.github.com>
2021-11-16 21:25:55 +0900

    [FIX] user_dataの重複登録を防ぐ

--- cogs/member_join.py
+++ cogs/member_join.py
@@ -13,8 +13,14 @@ class MemberJoin(commands.Cog):
             await member.add_roles(role)
             return
 
-        role = discord.utils.get(member.guild.roles, name="新人")
-        await member.add_roles(role)
+        newcomer = discord.utils.get(member.guild.roles, name="新人")
+        reported = discord.utils.get(member.guild.roles, name="MCID報告済み")
+
+        self.bot.cur.execute("SELECT COUNT(*) FROM user_data WHERE user_id = %s", (member.id,))
+        if self.bot.cur.fetchone()[0]:
+            await member.add_roles(reported)
+        else:
+            await member.add_roles(newcomer)
 
 
 def setup(bot):
